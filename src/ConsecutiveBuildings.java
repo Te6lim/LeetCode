20787dc11a73dbfa617c04bbadb2c8340b7aea16
te6lim <te6lim@gmail.com>
2024-10-01 12:25:47 +0100

    pass consecutive building problem

--- src/ConsecutiveBuildings.java
+++ src/ConsecutiveBuildings.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class ConsecutiveBuildings {
 
+    static String string = "00111111011110011000100100001000110001011110010100011111101100101110100";
+
     public static void main(String[] args) {
-        System.out.println(numberOfWays("0001100100"));
+        System.out.println(numberOfWays(string));
     }
 
     public static long numberOfWays(String s) {
@@ -12,158 +13,174 @@ public class ConsecutiveBuildings {
     }
 
     public static long totalOf101(String s) {
-        LinkedList<Integer> rightValues = new LinkedList<>();
-        int centerCount;
-        int lastCenterPosition = s.length();
-        int leftCount = 1;
-        int step = 0;
-        int rightCount;
+        ArrayList<Integer> rightValues = new ArrayList<>();
+        ArrayList<Integer> centerValues = new ArrayList<>();
+        int centerCount = 0;
+        int leftCount = 0;
+        int rightCount = 0;
         long total = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        int positionCounter = 0;
+
+        // find first
+        for  (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '1') {
-                step = i;
+                leftCount = 1;
+                positionCounter = i;
                 break;
             }
         }
 
-        while (step < s.length()) {
-            for (int i = step + 1; i < s.length(); i++) {
-                if (s.charAt(i) == '1') {
-                    leftCount++;
-                    step = i;
-                } else {
-                    break;
-                }
-            }
-
-            centerCount = 0;
-            for (int i = step + 1; i < s.length(); i++) {
-                if (s.charAt(i) == '0') {
-                    centerCount++;
-                    step = i;
-                    lastCenterPosition = i;
-                } else {
-                    break;
-                }
-            }
-
-            if (rightValues.isEmpty()) {
-                rightCount = 0;
-                for (int i = step + 1; i < s.length(); i++) {
-                    if (s.charAt(i) == '1') {
-                        rightCount++;
-                    } else {
-                        if (rightCount != 0) {
-                            rightValues.add(rightCount);
-                            rightCount= 0;
-                        }
-                    }
-                }
-
-                if (rightCount != 0) {
-                    rightValues.add(rightCount);
-                }
-            }
-
-            if (rightValues.isEmpty()) {
-                step = s.length();
+        for (int i = positionCounter + 1; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                leftCount++;
+                positionCounter = i;
             } else {
-                for (Integer rightValue : rightValues) {
-                    total += (long) leftCount * rightValue * centerCount;
-                }
-                rightValues.remove(0);
-                step = lastCenterPosition;
-                lastCenterPosition = s.length();
+                break;
             }
+        }
+
+        for (int i = positionCounter + 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                centerCount++;
+                positionCounter = i;
+            } else {
+                centerValues.add(centerCount);
+                centerCount = 0;
+                break;
+            }
+        }
+
+        if (centerCount > 0) {
+            centerValues.add(centerCount);
+            centerCount = 0;
+        }
+
+        for (int i = positionCounter + 1; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                if (centerCount > 0) {
+                    centerValues.add(centerCount);
+                    centerCount = 0;
+                }
+                rightCount++;
+            } else {
+                if (rightCount > 0) {
+                    rightValues.add(rightCount);
+                    rightCount = 0;
+                }
+                centerCount++;
+            }
+        }
+
+        if (centerCount > 0) {
+            centerValues.add(centerCount);
+        }
+
+        if (rightCount > 0) {
+            rightValues.add(rightCount);
+        }
+
+
+        long currentRightTotal = 0;
+        for (Integer rightValue : rightValues) {
+            currentRightTotal += (long) rightValue;
+        }
+        while (!centerValues.isEmpty()) {
+            if (!rightValues.isEmpty()) {
+                total += ((long) leftCount * centerValues.get(0)) * (currentRightTotal);
+                leftCount += rightValues.get(0);
+                positionCounter += rightValues.get(0);
+                currentRightTotal -= rightValues.get(0);
+                rightValues.remove(0);
+            }
+            positionCounter += centerValues.get(0);
+            centerValues.remove(0);
         }
         return total;
     }
 
     public static long totalOf010(String s) {
-        LinkedList<Integer> rightValues = new LinkedList<>();
+        ArrayList<Integer> rightValues = new ArrayList<>();
         ArrayList<Integer> centerValues = new ArrayList<>();
-        int centerCount;
-        int lastCenterPosition = s.length();
-        int leftCount = 1;
-        int step = 0;
-        int rightCount;
+        int centerCount = 0;
+        int leftCount = 0;
+        int rightCount = 0;
         long total = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        int positionCounter = 0;
+
+        // find first
+        for  (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '0') {
-                step = i;
+                leftCount = 1;
+                positionCounter = i;
                 break;
             }
         }
 
-        while (step < s.length()) {
-            if (leftCount == 0) {
-                for (int i = step + 1; i < s.length(); i++) {
-                    if (s.charAt(i) == '0') {
-                        leftCount++;
-                        step = i;
-                    } else {
-                        break;
-                    }
-                }
+        for (int i = positionCounter + 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                leftCount++;
+                positionCounter = i;
+            } else {
+                break;
             }
+        }
 
-            if (centerValues.isEmpty()) {
-                centerCount = 0;
-                for (int i = step + 1; i < s.length(); i++) {
-                    if (s.charAt(i) == '1') {
-                        centerCount++;
-                        step = i;
-                        lastCenterPosition = i;
-                    } else {
-                        break;
-                    }
-                }
+        for (int i = positionCounter + 1; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                centerCount++;
+                positionCounter = i;
+            } else {
                 centerValues.add(centerCount);
-            }
-
-            if (rightValues.isEmpty()) {
-                rightCount = 0;
                 centerCount = 0;
-                for (int i = step + 1; i < s.length(); i++) {
-                    if (s.charAt(i) == '0') {
-                        if (centerCount != 0) {
-                            centerValues.add(centerCount);
-                            centerCount = 0;
-                        }
-                        rightCount++;
-                    } else {
-                        if (rightCount != 0) {
-                            rightValues.add(rightCount);
-                            rightCount = 0;
-                        }
-                        centerCount++;
-                    }
-                }
+                break;
+            }
+        }
 
+        if (centerCount > 0) {
+            centerValues.add(centerCount);
+            centerCount = 0;
+        }
+
+        for (int i = positionCounter + 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
                 if (centerCount != 0) {
-                    rightValues.add(centerCount);
+                    centerValues.add(centerCount);
+                    centerCount = 0;
                 }
-
+                rightCount++;
+            } else {
                 if (rightCount != 0) {
                     rightValues.add(rightCount);
+                    rightCount = 0;
                 }
+                centerCount++;
             }
+        }
 
-            if (rightValues.isEmpty()) {
-                step = s.length();
-            } else {
-                for (Integer rightValue : rightValues) {
-                    total += (long) leftCount * rightValue * centerValues.get(0);
-                }
+        if (centerCount > 0) {
+            centerValues.add(centerCount);
+        }
 
-                leftCount += centerValues.get(0);
+        if (rightCount > 0) {
+            rightValues.add(rightCount);
+        }
+
+        long currentRightTotal = 0;
+        for (Integer rightValue : rightValues) {
+            currentRightTotal += (long) rightValue;
+        }
+        while (!centerValues.isEmpty()) {
+            if (!rightValues.isEmpty()) {
+                total += ((long) leftCount * centerValues.get(0)) * (currentRightTotal);
+                leftCount += rightValues.get(0);
+                positionCounter += rightValues.get(0);
+                currentRightTotal -= rightValues.get(0);
                 rightValues.remove(0);
-                centerValues.remove(0);
-                step = lastCenterPosition;
-                lastCenterPosition = s.length();
             }
+            positionCounter += centerValues.get(0);
+            centerValues.remove(0);
         }
         return total;
     }
